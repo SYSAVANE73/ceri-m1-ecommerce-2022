dda14e75292dd5059106d61b855058a40d37448e
IlonaFresu <56633023+IlonaFresu@users.noreply.github.com>
2022-10-19 17:57:50 +0200

    Création de la BD, routes et 1ere requête fastAPI

--- backend/src/songs.py
+++ backend/src/songs.py
@@ -1,6 +1,7 @@
 from typing import Optional
-
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Session, create_engine, select
+from fastapi import FastAPI
+import uvicorn
 import datetime
 
 class Artiste(SQLModel, table=True):
@@ -20,4 +21,34 @@ class Chanson(SQLModel, table=True):
 	titre: str
 	duree: float
 
+engine = create_engine("sqlite:///database.db")
+SQLModel.metadata.create_all(engine)
 
+#ROUTES
+def get_artistes():
+	with Session(engine) as session:
+		route = select(Artiste)
+		artistes = session.exec(route)
+		#for artiste in artistes:
+		#	print(artiste)
+def get_songs_by_album(albumId):
+	with Session(engine) as session:
+		route = select(Chanson).where(Chanson.num_album == albumId)
+		album = session.exec(route)
+		#for chanson in album:
+		#	print(chanson)
+
+def get_albums_by_artiste(artisteId):
+	with Session(engine) as session:
+		route = select(Album).where(Album.artiste_id == artisteId)
+		albums = session.exec(route)
+		#for album in albums:
+		#	print(album)
+
+#RECUPERER MUSIQUES
+app = FastAPI(title="Magasin de vinyles")
+@app.get("/")
+async def index():
+	return {"message" : "accueil"}
+if __name__ == "__main__":
+	uvicorn.run(app, host="127.0.0.1", port=8000)
