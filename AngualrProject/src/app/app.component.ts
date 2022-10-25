7a434e66062ec6379febc0a29447450ca4032dea
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2022-10-25 12:37:54 +0200

    ajout de service et liaison avec le backend

--- AngualrProject/src/app/app.component.ts
+++ AngualrProject/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GetDataService } from './services/get-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [GetDataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  service : GetDataService;
   title = 'Ecommerce';
+
+  constructor(_service:GetDataService, _http:HttpClient){
+    this.service = _service;
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
