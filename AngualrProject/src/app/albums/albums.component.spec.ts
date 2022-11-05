e841f1dec6274aff80c5455bcf38e7f7030a0ab0
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2022-11-05 22:16:38 +0100

    ajout de docker compose

--- AngualrProject/src/app/albums/albums.component.spec.ts
+++ AngualrProject/src/app/albums/albums.component.spec.ts
@@ -1,13 +1,27 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlbumsComponent } from './albums.component';
 
+
 describe('AlbumsComponent', () => {
   let component: AlbumsComponent;
   let fixture: ComponentFixture<AlbumsComponent>;
+  
+  let element = {
+    id : 2,
+    annee : '2002',
+    photo : 'djjjjjj'
+  }
+
+  let albumsTab : any = [
+    element
+  ]
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ AlbumsComponent ]
     })
     .compileComponents();
@@ -20,4 +34,9 @@ describe('AlbumsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  /*
+  it('test de la methode getAlbums()', ()=> {
+    expect(component.getAlbums()).toEqual(albumsTab)
+  })
+  */
 });
--- AngualrProject/src/app/artiste/artiste.component.spec.ts
+++ AngualrProject/src/app/artiste/artiste.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ArtisteComponent } from './artiste.component';
 
 describe('ArtisteComponent', () => {
@@ -8,6 +8,7 @@ describe('ArtisteComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ ArtisteComponent ]
     })
     .compileComponents();
