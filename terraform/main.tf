3010de36ab858b489e16a6d9d1bbe487c059c6b8
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2022-12-14 16:07:57 +0100

    ajout terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   cloud {
     organization = "E-ecommerce"
@@ -13,19 +12,3 @@ provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe.west1"
 }
-=======
-terraform {
-  cloud {
-    organization = "E-ecommerce"
-
-    workspaces {
-      name = "redpanda"
-    }
-  }
-}
-
-provider "google" {
-  project = "ceri-m1-ecommerce-2022"
-  region = "europe.west1"
-}
->>>>>>> 12a0be982fbc6a04c8214f2be073571e795739fd
