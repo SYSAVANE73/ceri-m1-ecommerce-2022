1f6cbba4d098fb12cdad25489aa0e1c6430fc476
SYSAVANE73 <72132856+SYSAVANE73@users.noreply.github.com>
2022-12-29 11:31:58 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+
+provider "google" {
+  project = "ceri-m1-ecommerce-2022"
+  region  = "europe.west1"
+  credentials = var.tf_token
+}
+
+variable "tf_token" {
+  default = ""
+}
