82458f882ffdc32ad7cb4a584139472e45ab9272
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2023-01-03 17:38:38 +0100

    update

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -1,16 +1,18 @@
 resource "google_cloud_run_service" "frontend" {
   name     = "redpanda-frontend"
-  location = "europe.west1"
+  location = "europe-west1"
 
   template {
     spec {
       containers {
         image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/redpanda/frontend:1.3.2"
       }
+      /*
       env {
         name  = "url"
         value = google_cloud_run_service.frontend.status.0.url
       }
+      */
     }
   }
 
