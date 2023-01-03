45f7e287bee988319107edbf54579398cec3eb1d
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2023-01-03 17:47:42 +0100

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,93 @@ provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe-west1"
 }
+
+data "google_secret_manager_secret" "address" {
+    secret_id = "mysql_address"
+}
+
+data "google_secret_manager_secret" "database" {
+    secret_id = "mysql-database-redpanda"
+}
+
+data "google_secret_manager_secret" "password" {
+    secret_id = "mysql-password-redpanda"
+}
+
+resource "google_cloud_run_service" "backend" {
+  name     = "redpanda-backend"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/redpanda/backend:1.3.2"
+
+        env{
+            name = "DATABASE_ADDRESS"
+            value_from {
+                secret_key_ref {
+                    name = data.google_secret_manager_secret.address.secret_id
+                    key = "latest"
+                }
+            }
+        }
+
+        env{
+            name = "DATABASE_USERNAME"
+            value = "redpanda"
+        }
+
+        env{
+            name = "DATABASE_NAME"
+            value_from {
+                secret_key_ref {
+                    name = data.google_secret_manager_secret.database.secret_id
+                    key = "latest"
+                }
+            }
+        }
+
+        env{
+            name = "DATABASE_PASSWORD"
+            value_from {
+                secret_key_ref {
+                    name = data.google_secret_manager_secret.password.secret_id
+                    key = "latest"
+                }
+            }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+
+resource "google_cloud_run_service" "frontend" {
+  name     = "redpanda-frontend"
+  location = "europe-west1"
+
+  template {
+    spec {
+      containers {
+        image = "europe-west1-docker.pkg.dev/ceri-m1-ecommerce-2022/redpanda/frontend:1.3.2"
+      }
+      
+      env {
+        name  = "url"
+        value = google_cloud_run_service.backend.status.0.url
+      }
+      
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
