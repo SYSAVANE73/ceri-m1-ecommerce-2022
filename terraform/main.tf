05c4d8f720b5f7aa8624f89898917deb73fb7d07
SYSAVANE73 <72132856+SYSAVANE73@users.noreply.github.com>
2023-01-03 14:16:23 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,12 +8,12 @@ terraform {
   }
 }
 
+variable "CREDENTIALS" {
+  default = ""
+}
+
 provider "google" {
   project = "ceri-m1-ecommerce-2022"
   region  = "europe.west1"
   credentials = var.CREDENTIALS
-}
-
-variable "CREDENTIALS" {
-  default = ""
 }
