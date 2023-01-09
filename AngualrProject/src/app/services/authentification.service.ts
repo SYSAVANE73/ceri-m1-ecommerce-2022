1a906c674702cbd8e18da65673d0983ab619e789
SYSAVANE73 <ahmadou-dia.sy-savane@alumni.univ-avignon.fr>
2023-01-09 16:02:37 +0100

    update url frontend

--- AngualrProject/src/app/services/authentification.service.ts
+++ AngualrProject/src/app/services/authentification.service.ts
@@ -16,7 +16,9 @@ export class AuthentificationService {
     
     // la méthode renvoie un observable et un Object en données
     return Observable.create((observer: Subscriber<any>) => {
-      let url = 'http://127.0.0.1:8000/login/' + login + "/" + pwd;
+      //let url = 'http://127.0.0.1:8000/login/' + login + "/" + pwd;
+      let url = 'https://redpanda-frontend-mwjszocsqa-ew.a.run.app/' + login + "/" + pwd;
+      
       console.log(url);
       this._http.get<any>(url,{})
       .subscribe(
@@ -41,7 +43,8 @@ export class AuthentificationService {
     
     // la méthode renvoie un observable et un Object en données
     return Observable.create((observer: Subscriber<any>) => {
-      let url = 'http://127.0.0.1:8000/signin/' + nom + "_" + prenom+ "_" + login+ "_" + pwd;
+      //let url = 'http://127.0.0.1:8000/signin/' + nom + "_" + prenom+ "_" + login+ "_" + pwd;
+      let url = 'https://redpanda-frontend-mwjszocsqa-ew.a.run.app/' + nom + "_" + prenom+ "_" + login+ "_" + pwd;
       console.log(url);
       this._http.get<any>(url,{})
       .subscribe(
--- Dockerfile
+++ Dockerfile
@@ -23,7 +23,7 @@ COPY nginx.conf /etc/nginx/conf.d/configfile.template
 #WORKDIR /usr/share/nginx/html
 COPY --from=node /AngualrProject/dist/ecommerce /usr/share/nginx/html
 
-#ENV PORT 8080
+#ENV PORT=8081
 #ENV HOST 0.0.0.0
 EXPOSE 8081
 CMD sh -c "envsubst '\$PORT' < /etc/nginx/conf.d/configfile.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
